292adb3554bff47234d98949600e3b6e17449fb2
Rafi <mrkamin2@gmail.com>
2025-02-22 20:32:26 +0500

    Fix the bag

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import { getProjects } from "@/sanity/sanity-utils";
 
 export default async function Home() {
-
   const projects = await getProjects();
-  console.log("click", projects.name)
+
   return (
-   <div>
-    <p>the project gose here</p>
-    {projects.map((project) => {
-      <div key={project._id}>{project.name}</div>
-    })}
-   </div>
+    <div>
+      <p>The projects go here:</p>
+      {projects.map((project) => (
+        <div key={project._id}>{project.name}</div> // ✅ Fix: Properly returning JSX
+      ))}
+    </div>
   );
 }
-
