db6fe57aab570088dba9c3666973692c95508638
Rafi <mrkamin2@gmail.com>
2025-03-04 19:10:33 +0500

    Apply some style to the hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-
+import Hero from "./components/Hero";
 
 export default async function Home() {
   
-
   return (
-    <div>
-     <p>Hello I&apos;m</p>
-     <h1>Mohammad Rafi Amin</h1>
-     <p>Full-Stack Developer</p>
-     <div>
-      <button>Download CV</button>
-      <button>Lets Talk</button>
-     </div>
-     <p>I can help you build a product, feature, or website look through some of my work and experience if you have a project that needs coding, do not hesitate to contact me.</p>
-
-    </div>
+   <div>
+      <Hero />
+   </div>
   );
 }
